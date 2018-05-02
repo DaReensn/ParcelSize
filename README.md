0728f947fa5a9ad45491fadd486267745b2e708f
reen <reen@Reen-Laptop>
2018-05-02 12:29:16 +0200

    bla

---
+++ README.md
@@ -0,0 +1,2 @@
+# ci_cd-pipeline
+mehr spaß
---
+++ src/main/java/StartRestServer.java
@@ -0,0 +1,15 @@
+package main.java;
+
+import java.io.IOException;
+import com.sun.jersey.api.container.httpserver.HttpServerFactory;
+import com.sun.net.httpserver.HttpServer;
+
+@SuppressWarnings("restriction")
+public class StartRestServer {
+
+	public static void main(String[] args) throws IllegalArgumentException, IOException {
+		HttpServer server = HttpServerFactory.create( "http://localhost:8443/parcel" );
+		server.start();
+	}
+	
+}
